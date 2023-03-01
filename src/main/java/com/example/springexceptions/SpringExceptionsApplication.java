2ecdf552fcfc2b817ba7136a8cf5e681aaf88b94
Maks <max@moais.ru>
2023-03-01 15:36:29 +0300

    Initial commit

---
+++ src/main/java/com/example/springexceptions/SpringExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExceptionsApplication.class, args);
+	}
+
+}
